990640dbdae0274d552201b31d8b82ea808d56ca
Saint Wesonga <saint.wesonga@microsoft.com>
2026-01-06 16:00:04 -0700

    Revert exception handling changes (#45)
    * Revert "Do not report errors unless FAILED returns true for the exception code (#16)"
    
    This reverts commit 5fa147b3ccca878be1016fce5cd11bff0ab3ae52.
    
    * Revert "Perform error reporting for uncaught native exceptions (#11)"
    
    This reverts commit 07c3446d7a44cd8906b4c34c42eb08fb19c78cf0.
    
    * Revert "Fix bug causing missing core dumps on Windows AArch64"
    
    This reverts commit 3ef2c632b2441109615b789af476438a7fb19774.
    
    * Revert "Windows AArch64 safefetch implementation should not use structured exception handling"
    
    This reverts commit 62651d694cdff37d0df4b5017a3401a78012ac4e.

--- test/hotspot/jtreg/runtime/ErrorHandling/UncaughtNativeExceptionTest.java
+++ test/hotspot/jtreg/runtime/ErrorHandling/UncaughtNativeExceptionTest.java
@@ -64,7 +64,7 @@ public class UncaughtNativeExceptionTest {
         assertTrue(Files.exists(hsErrPath));
 
         Pattern[] positivePatterns = {
-            Pattern.compile(".*Internal Error \\(0xdeadbeef\\).*")
+            Pattern.compile(".*Internal Error \\(0x2a\\).*")
         };
         HsErrFileUtils.checkHsErrFileContent(hsErrFile, positivePatterns, null, true /* check end marker */, false /* verbose */);
     }
